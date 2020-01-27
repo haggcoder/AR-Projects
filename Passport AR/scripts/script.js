b85f8f35fb8963024b857855e297cb20fece5af9
singhjapneet <46783934+singhjapneet@users.noreply.github.com>
2020-01-27 18:14:05 +0530

    Add files via upload

---
+++ Passport AR/scripts/script.js
@@ -0,0 +1,15 @@
+
+const Diagnostics = require('Diagnostics');
+const Scene = require('Scene');
+
+
+const instruct_plane = Scene.root.find('instructions');
+
+
+var targetPlane =  Scene.root.find('targetTracker0');
+   var confidenceSub = targetPlane.confidence.monitor().subscribe(function (e) {
+       if (e.newValue == 'HIGH') {
+           confidenceSub.unsubscribe();
+           instruct_plane.hidden = true;
+       }
+   });
